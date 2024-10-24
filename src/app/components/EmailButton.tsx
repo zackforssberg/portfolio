1ed2185add6b9215e9f453fbbb1b90ac4c99d252
Zack Forssberg <zack.forssberg@iths.se>
2024-10-24 15:41:36 +0200

    added fade animation to the Email copied text

--- src/app/components/EmailButton.tsx
+++ src/app/components/EmailButton.tsx
@@ -1,40 +1,3 @@
-// "use client";
-
-// import React, { useState } from "react";
-
-// const EmailButton: React.FC = () => {
-//   const [copied, setCopied] = useState(false);
-
-//   const copyToClipboard = () => {
-//     const email = "zack.forssberg@gmail.com";
-//     navigator.clipboard
-//       .writeText(email)
-//       .then(() => {
-//         setCopied(true);
-//         setTimeout(() => setCopied(false), 2000);
-//       })
-//       .catch((err) => {
-//         console.error("Failed to copy email:", err);
-//       });
-//   };
-
-//   return (
-//     <div className="flex items-center	">
-//       <button
-//         onClick={copyToClipboard}
-//         className={`px-6 py-2 text-xl rounded-lg text-white font-semibold transition-colors duration-300 ${
-//           copied ? "bg-green-500" : "bg-blue-500 hover:bg-blue-600"
-//         }`}
-//       >
-//         Get my email
-//       </button>
-//       {copied && <p className="ml-2 text-green-600 text-xl">Email copied!</p>}
-//     </div>
-//   );
-// };
-
-// export default EmailButton;
-
 "use client";
 
 import React, { useState } from "react";
@@ -69,7 +32,11 @@ const EmailButton: React.FC = () => {
       >
         Get my email
       </button>
-      {copied && <p className="ml-2 text-green-600 text-xl">Email copied!</p>}
+      {copied && (
+        <p className="ml-2 text-green-600 text-xl animate-fade">
+          Email copied!
+        </p>
+      )}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ module.exports = {
     extend: {
       animation: {
         boink: 'boink 0.3s ease-in-out',
+        fade: 'fade 2s ease-in-out'
+      },
+      keyframes: {
+        boink: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(0.9)' },
+        },
+        fade: {
+          '0%': { opacity: '0' },  // Starts fully transparent
+          '15%': { opacity: '1' }, // Fades in quickly
+          '85%': { opacity: '1' }, // Stays visible most of the time
+          '100%': { opacity: '0' }, // Fades out at the end
+        },
       },
     },
   },
