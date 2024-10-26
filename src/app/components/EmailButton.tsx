73388b352ad1d68e8d963fb2d426c61fb2de1c46
Zack Forssberg <zack.forssberg@iths.se>
2024-10-26 18:41:27 +0200

    updated emailbutton color

--- src/app/components/EmailButton.tsx
+++ src/app/components/EmailButton.tsx
@@ -26,7 +26,7 @@ const EmailButton: React.FC = () => {
       <button
         onClick={copyToClipboard}
         className={`px-6 py-2 text-xl rounded-lg text-white font-semibold transition-colors duration-300
-          ${copied ? "bg-green-500" : "bg-blue-500 hover:bg-blue-600"} ${
+          ${copied ? "bg-green-500" : "bg-sky-700 hover:bg-sky-800"} ${
           boink ? "animate-boink" : ""
         }`}
       >
