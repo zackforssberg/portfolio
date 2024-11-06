c53e8234a37f9775444f4cf33254232e851a1e07
Zack Forssberg <zack.forssberg@iths.se>
2024-11-06 14:19:41 +0100

    added the footer to homepage, added links to the footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,35 @@
 import EmailButton from "./EmailButton";
 
 const Footer = () => {
-  return <EmailButton />;
+  return (
+    <div className="pl-4 pr-4 bg-emerald-400 min-h-64">
+      <EmailButton />
+
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/zack-forssberg-9b9711289/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/zackforssberg"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a href="" target="_blank" rel="noopener noreferrer"></a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Article from "./components/Article";
+import Footer from "./components/Footer";
 
 export default function Home() {
-  return <Article />;
+  return (
+    <>
+      <Article />
+      <Footer />
+    </>
+  );
 }
