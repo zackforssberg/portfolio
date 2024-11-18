7760306a0a09d8cefec43b425195e0b4ea7b0450
Zack Forssberg <zack.forssberg@iths.se>
2024-11-18 21:38:30 +0100

    worked on footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,8 +2,17 @@ import EmailButton from "./EmailButton";
 
 const Footer = () => {
   return (
-    <div className="pl-4 pr-4 bg-emerald-400 min-h-64">
-      <EmailButton />
+    <div className="pl-4 pr-4 pb-8 bg-zinc-500 min-h-64 mt-20">
+      <div className="pt-8">
+        <h2 className="text-2xl">Feel free to say hello</h2>
+        <p className="text-xl pt-3">
+          I&apos;m always eager to engage with like-minded people in the tech
+          world.
+        </p>
+      </div>
+      <div className="pt-10">
+        <EmailButton />
+      </div>
 
       <ul className="text-xl underline underline-offset-4 flex space-x-4 mt-6">
         <li>
