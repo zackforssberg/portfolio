678256a9075bbeb509ccb69c0789575ba25b8cb0
Zack Forssberg <zforssb1@volvocars.com>
2025-01-31 15:57:17 +0100

    updated img to next Image, two cols instead of three

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -1,15 +1,21 @@
 import projects from "./project.json"; // Import projects from the JSON file
-
+import Image from "next/image";
 import Link from "next/link";
 
 const MyProjects = () => {
   return (
     <div className="min-h-screen p-6">
       <h1 className="text-3xl font-bold mb-6">My Projects</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {projects.map((project, index) => (
           <div key={index} className="bg-white shadow-md p-4 rounded-lg">
-            <img src={project.imageURL} /> {/* update to next Image asap */}
+            <Image
+              src={project.imageURL}
+              alt={project.title}
+              width={1000}
+              height={600}
+              className="mb-4"
+            />
             <h2 className="text-xl font-semibold">{project.title}</h2>
             <p className="text-gray-700 mb-4">{project.description}</p>
             <Link
