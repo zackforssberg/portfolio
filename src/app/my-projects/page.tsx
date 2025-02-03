d813c4706f1c5618d23c0711b044fc4555a8c411
Zack Forssberg <zforssb1@volvocars.com>
2025-02-03 16:39:42 +0100

    updated spacing under projects

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const MyProjects = () => {
   return (
-    <div className="min-h-screen p-6">
+    <div className="h-fit p-6">
       <h1 className="text-3xl font-bold mb-6">My Projects</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {projects.map((project, index) => (
