1c604ef76d0e6b15618b5e8f79adb7da66c1598d
Zack Forssberg <zforssb1@volvocars.com>
2025-02-14 15:36:58 +0100

    added hr to the navbar to section it

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -17,77 +17,80 @@ export default function Navbar() {
       setMenuHeight(menuRef.current.scrollHeight);
     }
   }, []);
-
   return (
-    <nav className="bg-navbarcolor p-4 md:p-8 lg:p-12">
-      <div className="flex items-center justify-between">
-        {/* Logo */}
-        {/* <div className="text-white text-lg font-bold">
+    <>
+      <nav className="bg-navbarcolor p-4 md:p-8 lg:p-12">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          {/* <div className="text-white text-lg font-bold">
           <Link href="/">MyLogo</Link>
         </div> */}
 
-        {/* Hamburger Icon */}
-        <div className="md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-white focus:outline-none"
-            aria-label="Toggle menu"
-          >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+          {/* Hamburger Icon */}
+          <div className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              className="text-white focus:outline-none"
+              aria-label="Toggle menu"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            </svg>
-          </button>
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div className="hidden md:flex space-x-7 text-xl">
+            <Link
+              href="/"
+              className="text-white hover:underline underline-offset-8"
+            >
+              Home
+            </Link>
+            <Link
+              href="/projects"
+              className="text-white hover:underline underline-offset-8"
+            >
+              Projects
+            </Link>
+            <Link href="" className="text-white cursor-default">
+              Contact (coming soon)
+            </Link>
+          </div>
         </div>
 
-        <div className="hidden md:flex space-x-7 text-xl">
-          <Link
-            href="/"
-            className="text-white hover:underline underline-offset-8"
-          >
-            Home
-          </Link>
-          <Link
-            href="/projects"
-            className="text-white hover:underline underline-offset-8"
-          >
-            Projects
-          </Link>
-          <Link href="" className="text-white cursor-default">
-            Contact (coming soon)
-          </Link>
+        <div
+          ref={menuRef}
+          style={{
+            maxHeight: isOpen ? `${menuHeight}px` : "0px",
+          }}
+          className={`transition-all duration-500 ease-in-out overflow-hidden md:hidden absolute left-0 w-full bg-navbarcolor`}
+        >
+          <div className="flex flex-col space-y-4 mt-4 ml-4 pb-3">
+            <Link href="/" className="text-white" onClick={toggleMenu}>
+              Home
+            </Link>
+            <Link href="/projects" className="text-white" onClick={toggleMenu}>
+              Projects
+            </Link>
+            <Link href="" className="text-white" onClick={toggleMenu}>
+              Contact (coming soon)
+            </Link>
+          </div>
+          <hr className="mt-auto" />
         </div>
-      </div>
-
-      <div
-        ref={menuRef}
-        style={{
-          maxHeight: isOpen ? `${menuHeight}px` : "0px",
-        }}
-        className={`transition-all duration-500 ease-in-out overflow-hidden md:hidden absolute left-0 w-full bg-navbarcolor pb-3`}
-      >
-        <div className="flex flex-col space-y-4 mt-4 ml-4">
-          <Link href="/" className="text-white" onClick={toggleMenu}>
-            Home
-          </Link>
-          <Link href="/projects" className="text-white" onClick={toggleMenu}>
-            Projects
-          </Link>
-          <Link href="" className="text-white" onClick={toggleMenu}>
-            Contact (coming soon)
-          </Link>
-        </div>
-      </div>
-    </nav>
+      </nav>
+      <hr className="hidden md:flex md:ml-8 md:mr-8 lg:ml-12 lg:mr-12" />
+    </>
   );
 }
