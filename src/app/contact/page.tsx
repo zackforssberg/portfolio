0c5a4358735d93347343d8eba4f8875f7dfdff8f
Zack Forssberg <zforssb1@volvocars.com>
2025-02-18 16:19:49 +0100

    added margin below form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import { ContactForm } from "../components/ContactForm";
 
 const Contact = () => {
   return (
-    <div className="m-12">
+    <div className="m-12 mb-36">
       <ContactForm />
     </div>
   );
