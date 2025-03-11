df3138f29687f8e04db3e272cc3533ba8ab8bda8
Zack Forssberg <zforssb1@volvocars.com>
2025-03-11 23:15:42 +0100

    center contact form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import { ContactForm } from "../components/ContactForm";
 
 const Contact = () => {
   return (
-    <div className="m-12 mb-36">
+    <div className="flex justify-center m-12 mb-36">
       <ContactForm />
     </div>
   );
